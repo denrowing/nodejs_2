0023d32e46da0acddb4124bcebf9e1f8c57a16ea
Denys Burdin <denrowing@gmail.com>
2021-10-25 15:07:42 +0300

    init commit

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
 require('dotenv').config();
+const rateLimit = require("express-rate-limit");
 
-const {MONGO_CONNECT_URL, PORT} = require('./configs/config');
+
+const { NODE_ENV, ALLOWED_ORIGIN, MONGO_CONNECT_URL, PORT} = require('./configs/config');
+const ErrorHandler = require('./errors/ErrorHandler');
+
 
 const app = express();
 
@@ -10,6 +16,19 @@ mongoose.connect(MONGO_CONNECT_URL);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors({origin: _configureCors}));
+app.use(helmet());
+
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+}));
+
+
+if (NODE_ENV === 'dev') {
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+}
 
 const { authRouter, userRouter } = require('./routes');
 
@@ -27,3 +46,14 @@ app.use('*', (err, req, res, next) => {
 app.listen(PORT, () => {
     console.log(`App listen ${PORT}`);
 });
+
+function _configureCors(origin, callback) {
+    if (NODE_ENV === 'dev') {
+        return callback(null, true);
+    }
+    const whitelist = ALLOWED_ORIGIN.split(';');
+    if (whitelist.includes(origin)) {
+        return callback(new ErrorHandler('CORS is hot allowed'), false);
+    }
+    return callback(null, true);
+}
